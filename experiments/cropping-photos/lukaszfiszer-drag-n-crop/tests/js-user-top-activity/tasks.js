c6631af8c4b95ba7bc345e5baa5d569fb0f60a39
ops <odox700@gmail.com>
2018-02-05 00:17:21 -0500

    - Page "profile", section "user-top-activity". --- Download and experiment on cropping-photo-library from github.com.

---
+++ experiments/cropping-photos/lukaszfiszer-drag-n-crop/tests/js-user-top-activity/tasks.js
@@ -0,0 +1,20 @@
+// setUserTopActivityParameters();
+//
+// setPhotoHolderWidth();
+// setPhotoHolderHeight();
+//
+// var photos = [
+//     {id: 1, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 2, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 3, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 4, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 5, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 6, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"},
+//     {id: 7, photo_link: "https://farm5.staticflickr.com/4331/36248325990_3e9f1255b0_h.jpg"}
+// ];
+//
+// var numOfPhotos = photos.length;
+//
+// for (i = 0; i < numOfPhotos; i++) {
+//
+// }
