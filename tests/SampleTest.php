eeb02c57b9a629d05f7c96578840aa735c955ca0
ops <odox700@gmail.com>
2018-04-05 18:03:27 -0400

    - Run first phpunit test.

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -1,12 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ops
- * Date: 2018-04-05
- * Time: 17:19
- */
 
-class SampleTest
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
 {
 
+
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
+?>
